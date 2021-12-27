93ab5999a62980cf75fd75821fa13d8cb11a8d67
tealhoion87 <nealtoms@gmail.com>
2021-12-27 17:16:41 +0000

    Add base and home templates and wire up

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 if os.path.exists("env.py"):
     import env
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
